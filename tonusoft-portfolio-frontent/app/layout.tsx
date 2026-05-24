824fc27c04bc548c1e46ea8f53203febe0a74700
tonutonmoy <www.tonutonmoy12@gmail.com>
2026-05-24 23:37:43 +0600

    done

--- tonusoft-portfolio-frontent/app/layout.tsx
+++ tonusoft-portfolio-frontent/app/layout.tsx
@@ -7,6 +7,7 @@ import { createMetadata, siteUrl } from '../src/lib/seo';
 
 const inter = Inter({ subsets: ['latin'] });
 const googleAnalyticsId = 'G-T22TWK5BFJ';
+const fbPixelId = '1452707766613157';
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
@@ -23,6 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
 
+      {/* Google Analytics Script */}
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
         strategy="afterInteractive"
@@ -36,6 +38,32 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           gtag('config', '${googleAnalyticsId}');
         `}
       </Script>
+
+      {/* Facebook Pixel */}
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+
+          fbq('init', '${fbPixelId}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${fbPixelId}&ev=PageView&noscript=1`}
+        />
+      </noscript>
 
       <body className={inter.className} suppressHydrationWarning>
         <Providers>{children}</Providers>
